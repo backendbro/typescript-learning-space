7359f2871b6cb9d7256508f692f5c2767a8ddea1
backendbro <ukagha.nzubechukwu@yahoo.com>
2023-09-16 21:42:07 +0100

    Today's Practice So far

--- Lesson 7/dist/js/index.js
+++ Lesson 7/dist/js/index.js
@@ -4,4 +4,27 @@ const todaysTransactions = {
     Books: -30,
     Job: 22
 };
-console.log(todaysTransactions.Pizza);
+const todaysNet = (transactions) => {
+    let total = 0;
+    for (const transaction in transactions) {
+        total += transactions[transaction];
+    }
+    return total;
+};
+const student = {
+    name: "John Doe",
+    GPA: 12,
+    classes: [200, 300]
+};
+for (const key in student) {
+    console.log(`${key}:  ${student[key]}`);
+}
+for (const key in student) {
+    console.log(`${key}: ${student[key]}`);
+}
+for (const key in student) {
+    console.log(`${key}: ${student[key]}`);
+}
+const logStudentKey = (student, key) => {
+    console.log(`Student: ${student[key]}`);
+};
--- Lesson 7/src/index.ts
+++ Lesson 7/src/index.ts
@@ -1,10 +1,17 @@
 // INDEX SIGNATURE 
 
+// interface TransactionObj {
+//     [index:string]:number // This indicates an index/key of string 
+//                           // and returns a number
+// }
+
 interface TransactionObj {
+    readonly [index:string]:number 
     Pizza:number,
-    Books:number, 
-    Job:number 
+    Books:number,
+    Job:number
 }
+
 
 const todaysTransactions: TransactionObj = {
     Pizza:-10,
@@ -12,4 +19,46 @@ const todaysTransactions: TransactionObj = {
     Job:22
 }
 
-console.log(todaysTransactions.Pizza)
+
+const todaysNet = (transactions: TransactionObj) :number => {
+    let total = 0 
+    for (const transaction in transactions ){
+        total+=transactions[transaction]
+    }
+    return total
+} 
+
+/////////////////////////////////////////////
+interface Student {
+    [key:string]:number | string | number[] | undefined
+    name:string 
+
+    GPA:number, 
+    classes?:number[]
+}
+
+const student: Student = {
+    name:"John Doe",
+    GPA:12, 
+    classes:[200, 300]
+}
+
+
+for (const key in student) {
+    console.log(`${key}:  ${student[key]}`)
+}
+
+// ITERATING WITHOUT SPECIFYING AN INDEX SIGNATURE 
+for (const key in student ) {
+    console.log(`${key}: ${student[key as keyof Student]}`)
+}
+
+
+for (const key in student) {
+    console.log(`${key}: ${student[key as keyof typeof student]}`)
+}
+
+const logStudentKey = (student:Student, key: keyof Student) : void  => {
+    console.log(`Student: ${student[key]}`)
+}
+
